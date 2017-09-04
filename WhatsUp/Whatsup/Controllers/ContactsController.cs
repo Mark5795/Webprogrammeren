2a3410ae96932de4f61a3b385a238449bc76018e
Matk5795 <markkea95@gmail.com>
2017-09-04 16:22:37 +0200

    Start

---
+++ WhatsUp/Whatsup/Controllers/ContactsController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Whatsup.Controllers
+{
+    public class ContactsController : Controller
+    {
+        // GET: Contacts
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Nieuw Contact
+        public ActionResult Nieuw()
+        {
+
+            return View();
+        }
+
+        //  Contact
+        public ActionResult Weizig(int? id)
+        {
+
+            return View();
+        }
+
+        // Delete Contact
+        public ActionResult Delete(int? id)
+        {
+
+            return View();
+        }
+    }
+}
