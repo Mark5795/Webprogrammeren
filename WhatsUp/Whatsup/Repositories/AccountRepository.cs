a79b9bc5416a5401629933464a5fdbb5748c4685
Matk5795 <markkea95@gmail.com>
2017-10-04 11:18:01 +0200

    yess

--- WhatsUp/Whatsup/Repositories/AccountRepository.cs
+++ WhatsUp/Whatsup/Repositories/AccountRepository.cs
@@ -10,14 +10,13 @@ namespace Whatsup.Repositories
     public class AccountRepository : IAccountRepository
     {
         private WhatsupContext db = new WhatsupContext();
-        //public IEnumerable<Account> Account { get { return db.Accounts; } }
-        public IEnumerable<Account> Account { get { return null; } }
+        public IEnumerable<Account> Account { get { return db.Accounts; } }
+        //public IEnumerable<Account> Account { get { return null; } }
 
         public Account GetAccount(string EmailAddress, string Password)
         {
-            //Account account = db.Contacts.Find(EmailAddress, Password);
-            //return account;
-            return null;
+            Account account = db.Accounts.Find(EmailAddress, Password);
+            return account;
         }
     }
 }
