1fc093c7a03ce721fff23f2bab77ed0322e7737e
Mark5795 <markkea95@gmail.com>
2018-11-10 15:05:50 +0100

    Begonnen met groupchat

---
+++ WhatsUp/Whatsup/Models/AddGroupViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Whatsup.Models
+{
+    public class AddGroupViewModel
+    {
+        public string Name { get; set; }
+
+        public AddGroupViewModel() { }
+
+        public AddGroupViewModel(string name)
+        {
+            Name = Name;
+        }
+    }
+}
